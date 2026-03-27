1f40be7c19ded1f2211ed9124a445910e910c3d8
sabareeshk886 <sabareeshk886@gmail.com>
2026-03-27 23:30:47 +0530

    fix(db): add ssl and prepare config for supabase connection

--- db/index.ts
+++ db/index.ts
@@ -5,5 +5,7 @@ import * as schema from './schema';
 
 const connectionString = process.env.DATABASE_URL!;
 
-const client = postgres(connectionString);
+// Disable prepare as it is not supported for "Transaction" pool mode
+// Enforce SSL for Supabase connections
+const client = postgres(connectionString, { prepare: false, ssl: 'require' });
 export const db = drizzle(client, { schema });
